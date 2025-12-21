879d6f872d64e07589f4fcbe641c39395bf17d80
salman <khattaksd@gmail.com>
2025-12-21 14:54:32 -0500

    Add .ts extensions to imports for Deno compatibility

--- mod.ts
+++ mod.ts
@@ -1,6 +1,6 @@
-export { parse } from './src/parser/parse-webvtt';
+export { parse } from './src/parser/parse-webvtt.ts';
 export {
   DiagnosticSeverity,
   DiagnosticCode,
   formatDiagnostics,
-} from './src/parser/diagnostics';
+} from './src/parser/diagnostics.ts';
